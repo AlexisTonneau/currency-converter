643121bc8685e87029f6669a8c49215a0b24ba4c
Alexis Tonneau <atonneau@juniorisep.com>
2021-10-13 17:23:45 -0700

    With local json file

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import uvicorn as uvicorn
@@ -8,8 +9,12 @@ import requests
 app = FastAPI()
 
 api_key = os.getenv('API_KEY')
-countries = requests.get(
-    'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-currency-code.json').json()
+with open('country-by-currency-code.json') as f:
+    countries = json.load(f)
+
+for ctr in countries:
+    if ctr['country'] == 'France':
+        assert ctr['currency_code'] == 'EUR'
 
 
 @app.get('/')
@@ -21,12 +26,14 @@ async def get_conversion(country: str, number: float, native_currency: str = 'EU
     if not currency:
         raise HTTPException(status_code=400, detail='Country not found')
 
-    resp = requests.get(f'https://free.currconv.com/api/v7/convert?q={currency}_{native_currency}&apiKey={api_key}&compact=ultra')
+    resp = requests.get(
+        f'https://free.currconv.com/api/v7/convert?q={currency}_{native_currency}&apiKey={api_key}&compact=ultra')
     if not resp.ok:
         raise HTTPException(status_code=resp.status_code, detail=resp.text)
     try:
-        return Response(content=str(round(float(resp.json()[f'{currency}_{native_currency}'])*number, 2)), media_type="text"
-                                                                                                              "/plain")
+        return Response(content=str(round(float(resp.json()[f'{currency}_{native_currency}']) * number, 2)),
+                        media_type="text"
+                                   "/plain")
     except Exception as e:
         raise HTTPException(status_code=500, detail=e)
 
