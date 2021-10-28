9d9792831c126ca75ec6212d9df4ac78222b72c8
Alexis Tonneau <atonneau@juniorisep.com>
2021-10-28 14:44:49 -0700

    Handle currency sign in API

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ api_key = os.getenv('API_KEY')
 with open('country-by-currency-code.json') as f:
     countries = json.load(f)
 
+with open('Common-Currency.json') as f:
+    complete_curr = json.load(f)
+
 for ctr in countries:
     if ctr['country'] == 'France':
         assert ctr['currency_code'] == 'EUR'
@@ -40,6 +43,31 @@ async def get_conversion(country: str, number: float, native_currency: str = 'EU
         raise HTTPException(status_code=resp.status_code, detail=resp.text)
     try:
         return Response(content=str(round(float(resp.json()[f'{currency}_{native_currency}']) * number, 2)),
+                        media_type="text"
+                                   "/plain")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
+
+
+@app.get('/1.4')
+async def get_conversion(country: str, number: float, native_currency: str = 'EUR'):
+    native_currency = native_currency.upper()
+    if native_currency not in currencies:
+        return Response(media_type="text/plain", content=f"{native_currency} not recognized")
+    currency = None
+    for ctr in countries:
+        if ctr['country'] == country:
+            currency = ctr['currency_code']
+    if not currency:
+        return Response(media_type="text/plain", content=f"{country} not recognized")
+
+    resp = requests.get(
+        f'https://free.currconv.com/api/v7/convert?q={currency}_{native_currency}&apiKey={api_key}&compact=ultra')
+    if not resp.ok:
+        raise HTTPException(status_code=resp.status_code, detail=resp.text)
+    try:
+        return Response(content=str(round(float(resp.json()[f'{currency}_{native_currency}']) * number, 2)) +
+                                complete_curr[native_currency]['symbol_native'],
                         media_type="text"
                                    "/plain")
     except Exception as e:
